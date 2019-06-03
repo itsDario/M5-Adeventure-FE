0b567b110dde8f55fcc1d8525a18c4a654e61027
github username <itsDario1@gmail.com>
2019-06-03 13:30:32 -0400

    initial commit of project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+// import BotsPage from "./containers/BotsPage";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {/* <BotsPage /> */}
+        Hello World
+      </div>
+    );
+  }
+}
+
+export default App;
