13c1e95e7581a9fb617062c3bf7a88b4eb5fad73
github username <itsDario1@gmail.com>
2019-06-20 13:58:46 -0400

    a

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 //server.js
 
-const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
+import express, { static } from 'express';
+import favicon from 'express-favicon';
+import { join } from 'path';
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(static(__dirname));
+app.use(static(join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
     return res.send('pong');
 });
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port);
